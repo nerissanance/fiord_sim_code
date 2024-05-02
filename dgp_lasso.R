5a8ee504859d9db386f2bfe3accc5319287eb197
nerissanance <nerissanance@gmail.com>
2024-05-01 18:58:57 -0700

    adding dgp generation with LASSO model and interaction terms

--- dgp_lasso.R
+++ dgp_lasso.R
@@ -8,20 +8,27 @@ library(pROC)
 library(foreach)
 library(tableone)
 library(glmnet)
+library(doParallel)
+library(doRNG)
+
+#(cl <- makeCluster(detectCores()))
+#registerDoParallel(cl)
+
 
 dt <-data.table(read.csv("./data/fakedataset.csv"))
 dt <-dt[,-c("X")]
 
 R=100 #number of dataset repetitions
+#registerDoRNG(seed = 123)
 set.seed(200)
 
-simdata_list <-foreach(j=1:R)%do%{
+simdata_list <-foreach(j=1:R)%do%{ #%dopar%
+
   print(paste0("begin iteration: ",j))
   train_index <- as.logical(rbinom(n=nrow(dt),size=1,prob=0.1))
-  train <- as.amtrix(dt[train_index==FALSE])
-  test <- as.matrix(dt[train_index==TRUE])
+  train <- (dt[train_index==FALSE])
+  test <- (dt[train_index==TRUE])
 
-  #simdata <- dt[,.(age_base,sex,ie_type,code5txt,quartile_income)]
   simdata <- dt[sample(nrow(dt),nrow(train),replace=F),.(age_base,sex,ie_type,code5txt,quartile_income)]
   preds <- data.frame(matrix(nrow=nrow(simdata),ncol=length(dt)-5))
   models <- as.list(names(dt)[6:length(names(dt))])
@@ -32,29 +39,35 @@ simdata_list <-foreach(j=1:R)%do%{
   for(out in 1:(length(names(dt))-5)){
     (outcome <- names(dt)[out+5])
     (predictors <- names(train)[1:(grep(outcome,names(train))-1)])
-    (thisformula <- as.formula(paste0(outcome,"~",paste0(predictors,collapse="+"))))
-    glmnet_model <- cv.glmnet(thisformula, alpha=0, family='binomial',data=train)
+
+    #create model matrix which includes all pairwise interaction terms
+    x <- model.matrix(as.formula( ~ .^2),train[,predictors,with=F])
+
+    glmnet_model <- cv.glmnet(x=x,y=as.matrix(train[,outcome,with=F]), alpha=0, family='binomial')
 
     mses <- matrix(nrow=length(cutoffs))
 
-    foreach(j=1:length(cutoffs))%do%{
-      pred_j <- as.numeric(predict(glmnet_model,newdata=simdata,type='response')>cutoffs[j])
-      mses[j] <- (sum(train[[outcome]]-pred_j)^2)/nrow(dt)
+    foreach(k=1:length(cutoffs))%do%{
+      pred_k <- as.numeric(predict(glmnet_model,newx=model.matrix(as.formula( ~ .^2),simdata),
+                                   type='response')>cutoffs[k])
+      mses[k] <- (sum(train[[outcome]]-pred_k)^2)/nrow(dt)
     }
-    # ##select the first one that minimizes auc
-    simdata[,outcome] <- as.numeric(predict(glm_model,
-                                            newdata=simdata,type='response')>cutoffs[min(grep(min(mses),mses))])
+    # ##select the first cutoff that minimizes auc
+    simdata[,outcome] <- as.numeric(predict(glmnet_model,
+                                            newx=model.matrix(as.formula( ~ .^2), simdata),
+                                            type='response')>cutoffs[min(grep(min(mses),mses))])
 
   }
   return(simdata)
 
 }
 
+#stopCluster(cl)
 
 
 ##correlation plot
 corrplot(cor(as.matrix(dt)), method = "color")
-for(i in 1:3){
+for(i in 1:length(simdata_list)){
   corrplot(cor(as.matrix(simdata_list[[i]])), method = "color")
 }
 
@@ -65,8 +78,6 @@ CreateContTable(data=preds,vars = names(simdata)[6:20])
 
 #transport::wasserstein(dt$age_base, simdata_list[[1]]$age_base, p=1, tplan=NULL, costm=NULL, prob=TRUE)
 #waddR::
-
-
 
 ## find truth
 outcome <- "mace_hf_4"
@@ -82,8 +93,3 @@ mean(predict(glm_model,newdata=dt_a1,type='response'))
 mean(predict(glm_model,newdata=dt_a0,type='response'))
 
 -1/(1+exp(-sum(glm_model$coefficients[names(glm_model$coefficients)[(grepl("glp",names(glm_model$coefficients)))]])))
-
-
-
-
-

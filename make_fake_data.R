1f4fd49e0cbab2e0dbf484a296f5e494772520c7
nerissanance <nerissanance@gmail.com>
2024-05-21 10:55:39 -0700

    add varying time length to fake data generation

--- make_fake_data.R
+++ make_fake_data.R
@@ -1,11 +1,14 @@
-
+library(lava)
 coefs <-  data.table::fread("../../cvd_simulation/data/coefficients.txt")
 
 source("../../cvd_simulation/functions/synthesizeDD.R")
 source("../../cvd_simulation/functions/sim_sig_data.R")
 data_specs <- synthesizeDD(coefs)
-N_time=4
+N_time=6
+set.seed(255)
 dt <- data.table(sim_sig_data(data_specs,
                               n=60000,
                               N_time=N_time,
                               iter = 1)[[1]])
+names(dt)
+write_csv(dt,paste0("./data/fakedataset_t",N_time,".csv"))

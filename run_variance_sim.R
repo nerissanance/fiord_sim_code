5194a3403a55b85ae3213902500133e48c60f093
nerissanance <nerissanance@gmail.com>
2024-07-08 07:46:36 +0200

    update to compare tmle and ic inference

--- run_variance_sim.R
+++ run_variance_sim.R
@@ -3,16 +3,20 @@
 #   -the final analytic strategy specs for n iterations to get empirical IC for R iterations
 #   -a non-parametric bootstrap of n_boot iterations on each estimate of R
 library(ltmle)
+library(doParallel)
+library(tidyverse)
+library(SuperLearner)
+library(data.table)
+library(doRNG)
 
 #import functions
 sapply(paste0("./functions/",list.files(path="./functions/")),source,.GlobalEnv)
 
-####SET PARAMS: 
+####SET PARAMS:
 model <- "glm"
-#bootstrap iterations
 reps <- 200
-N_time <- 4 
-n_boot <- 2#00
+N_time <- 4
+n_boot <- 2#00 #bootstrap iterations
 final_lib <- c("SL.mean","glm","SL.glm.interaction","SL.earth","SL.xgboost","SL.randomForest")
 trunc_level <- c(0.001,1)
 ####END PARAMS
@@ -22,9 +26,38 @@ trunc_level <- c(0.001,1)
 dataname <-paste0("simdata_list_",as.character(reps),"_","t",as.character(N_time),"_",model)
 simdata_list <- readRDS(paste0("./tmp/",dataname,".RDS"))
 
+### register paralleization and set seed
+(cl <-makeCluster(detectCores()-2))
+registerDoParallel(cl)
+registerDoRNG(seed = 123)
 
-try(result <- run_ltmle(data=simdata_list[[i]], 
-                        SL.library=SL.library, 
-                        trunc_level=specs$trunc_levels[j,],
-                        varmethod="tmle"))
 
+ci_df <- foreach(i = 1:length(simdata_list),.combine='rbind') %dopar% {
+    library(ltmle)
+    try(result1 <- run_ltmle(data=simdata_list[[i]],
+                             SL.library=final_lib,
+                             trunc_level=trunc_level,
+                             varmethod="ic"))
+    try(result2 <- run_ltmle(data=simdata_list[[i]],
+                             SL.library=final_lib,
+                             trunc_level=trunc_level,
+                             varmethod="tmle"))
+
+    try(sum1 <- summary(result1))
+    try(sum2 <- summary(result2))
+    if(!is.null(sum1)){
+        ic_ci <- cbind("ic", sum1$effect.measures$ATE$estimate, sum1$effect.measures$ATE$CI)
+    }else{
+      ic_ci <- c("ic","","")
+    }
+  if(!is.null(sum2)){
+      tmle_ci <- cbind("tmle", sum2$effect.measures$ATE$estimate, sum2$effect.measures$ATE$CI)
+    }else{
+    tmle_ci <- c("tmle","","")
+  }
+
+  return(data.frame(rbind(ic_ci,tmle_ci)))
+  }
+stopCluster(cl)
+
+saveRDS(data.frame(ci_df),paste0("./tmp/ci_est_",dataname,".RDS"))
